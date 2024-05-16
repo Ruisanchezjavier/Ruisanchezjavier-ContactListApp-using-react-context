1079514c1f9fd942baeb87da9e522efd5bb5fb77
Ruisanchezjavier <160360911+Ruisanchezjavier@users.noreply.github.com>
2024-05-16 01:20:20 +0000

    continuando

---
+++ src/js/component/ContactCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+
+const ContactCard = ({name, address, phone, email}) => {
+    return (
+        <>
+        <div>
+            <div className="contact">
+                <img src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+                <div className="contact-info">
+                    <p className="contact-name">{name}</p>
+                    <p className="contact-phone">{address}</p>
+                    <p className="contact-address">{phone}</p>
+                    <p className="contact-email">{email}</p>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default ContactCard;
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,18 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -24,20 +13,63 @@ const getState = ({ getStore, getActions, setStore }) => {
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
+
+			// GET Single Agenda
+			loadAgendaContacts: async () => {
+				const response = await fetch("https://playground.4geeks.com/contact/agendas/JavierRuisanchez/contacts")
+				if(!response.ok) {
+					throw new Error(response.status, response.statusText)
+				}
+				const data = await response.json();
+				setStore({contacts: data.contacts});
+			},
+            
+			//You will need functions to:
+			//POST new contacts trough the API
+            createNewContact: async () => {
+				const response = await fetch("https://playground.4geeks.com/contact/agendas/JavierRuisanchez/contacts", {
+					method: "POST",
+					body: JSON.stringify(contactObject),
+					headers: {
+						'Content-Type': 'application/json'
+					}
 				});
+				if (response.ok) {
+					const data = await response.json();
+					return data;
+				} else {
+					console.log('error: ', response.status, response.statusText);
+					return {error: {status: response.status, statusText: response.statusText}};
+				}
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
+
+			//PUT (update) contacts through the API
+			
+			// DELETE contacts
+			deleteContact: async (contactId) => {
+				const response = await fetch(`https://playground.4geeks.com/contact/agendas/JavierRuisanchez/contacts/${contactId}`, {
+					method: "DELETE",
+				});
+				if(!response.ok) {
+                      throw new Error(response.status, response.statusText)
+				}
+                 getActions().loadAgendaContacts();
+			},
 		}
 	};
 };
